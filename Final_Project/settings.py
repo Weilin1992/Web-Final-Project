e3b757727af3329c5f4e01c851b3aab760103146
Weilin1992 <wz196@rutgers.edu>
2016-04-20 19:54:48 -0400

    set mysql to django

--- Final_Project/settings.py
+++ Final_Project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Stock'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'Final_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Stock',
+        'USER': 'root',
+        'PASSWORD': '1111',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
