c2f443b98343ee4c3094394d92250c7f01d3350a
Weilin1992 <wz196@rutgers.edu>
2016-04-26 20:53:50 -0400

    add lowest price company query

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns,url
 from . import views
 
 urlpatterns = [
+    url(r'^lessthan/$',views.lesserthan,name='lessstock'),
+    url(r'^lowest/$',views.lowestprice,name='loweststock'),
+    url(r'^average/$',views.averagestock,name='averagestock'),
     url(r'^highestLastnDays/$',views.highestLastnDays,name='highestLastnDays'),
     url(r'^latestOneyearstock/$',views.latestOneyearstock,name='lastestoneyearstock'),
     url(r'^onedaystock/$',views.onedaystock,name='onedaystock'),
--- api/views.py
+++ api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.db import connection
+import json
+import collections
 
 from stockPrediction.models import *
 from .serializers import *
@@ -44,9 +47,48 @@ def latestOneyearstock(request):
 def highestLastnDays(request):
     company_name = str(request.GET.get('name'))
     days = int(request.GET.get('day'))
-    print days
+    #print days
     stocks = Oneyearstock.objects.raw('select * from stockPrediction_oneyearstock where name= %s and datediff(CURDATE(),time) <= %s ORDER BY CLOSE DESC limit 1',[company_name,days])
     serializers = OneyearstockSerializer(stocks,many=True)
     return Response(serializers.data)
 
+@api_view(['GET'])
+def averagestock(request):
+    company_name = str(request.GET.get('name'))
+    cursor = connection.cursor()
+    cursor.execute('select avg(close) from stockPrediction_oneyearstock WHERE name = %s',[company_name])
+    average = cursor.fetchone()[0]
+    rowlist =[]
+    d = collections.OrderedDict()
+    d["name"] = company_name
+    d["price"] = average
+    rowlist.append(d)
+    return Response(rowlist)
+
+@api_view(['GET'])
+def lowestprice(request):
+    cursor = connection.cursor()
+    cursor.execute('select min(close),NAME FROM stockPrediction_oneyearstock GROUP BY NAME ')
+    rowlist = []
+    for row in cursor.fetchall():
+        d = collections.OrderedDict()
+        d["name"] = row[1]
+        d["price"] = row[0]
+        rowlist.append(d)
+    return Response(rowlist)
+
+@api_view(['GET'])
+def lesserthan(request):
+    company_name = str(request.GET.get('name'))
+    cursor = connection.cursor()
+    cursor.execute('select min(close) from stockPrediction_oneyearstock WHERE NAME = %s',[company_name])
+    price = cursor.fetchone()[0]
+    cursor.execute('select DISTINCT C.id,C.name FROM stockPrediction_oneyearstock as Y,stockPrediction_company as C WHERE Y.name=C.name and CLOSE < %s',[price])
+    rowlist = []
+    for row in cursor.fetchall():
+        d = collections.OrderedDict()
+        d["id"] = row[0]
+        d["name"] = row[1]
+        rowlist.append(d)
+    return Response(rowlist)
 
